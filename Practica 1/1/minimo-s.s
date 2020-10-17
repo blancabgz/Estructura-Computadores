3f469ccab74d3395c03657cf459945751638de6a
blancabgz <blancabrilgonz@correo.ugr.es>
2020-10-17 18:42:44 +0200

    Ejercicio 1 y 2

--- Practica 1/1/minimo-s.s
+++
@@ -1,8 +0,0 @@
-.globl _start
-
-.text
-
-_start:
-	movl  $1, %eax    # exit
-        xorl  %ebx, %ebx  # 0
-        int   $0x80       # llamada a exit
--- Practica 1/1/printf-c-1.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-	int i = 12345;
-	printf("i=%d\n", i);
-	return 0;
-}
